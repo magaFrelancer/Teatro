37d0434ad422b8afad7e8c8875daaf4699ceb3d4
magaFrelancer <65971702+magaFrelancer@users.noreply.github.com>
2020-07-31 21:52:18 +0300

    Начало
    Сделал всё что нужно в header-е

---
+++ Teatro/script.js
@@ -0,0 +1,24 @@
+let btns = document.querySelectorAll('.info-right__btn'),
+    imgs = document.querySelectorAll('.info-right__itms'),
+    index = 1;
+
+
+function target() {
+    let atr = this.getAttribute('data-target');
+    imgs[index].classList.remove('active')
+
+    if (atr == 'prev') {
+        index--;
+        if (index < 0) {
+            index = imgs.length - 1
+        }
+    } else if (atr == 'next') {
+        index++;
+        if (index >= imgs.length) {
+            index = 0;
+        }
+    }
+    imgs[index].classList.add('active');
+}
+
+btns.forEach(item=> {item.addEventListener('click',target)})
